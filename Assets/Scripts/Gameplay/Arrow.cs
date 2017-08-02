7798d8c2ea85802b5297068e06152bf2dc1387c9
Luke <luke@lukecritchley.com>
2017-08-02 15:58:32 +0100

    020817: added new features for the arrow launcher, including the arrow that is going to be launched as a prefab

--- Assets/Scripts/Gameplay/Arrow.cs
+++ Assets/Scripts/Gameplay/Arrow.cs
@@ -6,15 +6,31 @@ public class Arrow : MonoBehaviour {
 
     public GameObject arrowShooter;
     public GameObject arrowEnd;
+    public GameObject arrowPrefab;
 
     public Vector3 startPosition;
     public Vector3 endPosition;
 
+    //enum to determine which way to fire the arrow
+    public enum arrowDirection
+    {
+        Left,
+        Right,
+        Up, 
+        Down
+    }
+
+    public arrowDirection aDirection;
+
 	// Use this for initialization
 	void Start () 
     {
+        arrowShooter = gameObject;
+        arrowEnd = transform.GetChild(0).gameObject;
         startPosition = arrowShooter.transform.position;
         endPosition = arrowEnd.transform.position;
+        arrowPrefab = Instantiate(Resources.Load("Prefabs/Arrow")) as GameObject;
+        arrowPrefab.transform.position = startPosition;
 		
 	}
 	
@@ -22,6 +38,16 @@ public class Arrow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-		
+        FireArrow();
 	}
+
+    void FireArrow()
+    {
+        switch(aDirection)
+        {
+            case arrowDirection.Left:
+                break;
+        }
+    }
+
 }
